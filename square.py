f73a6b2ec70e902ba6959f1a6bca872e18ffb337
0FAUXS <lumin.lumin@du.edu>
2023-01-15 17:39:23 -0800

    def of sigmoid with lambda square

--- square.py
+++ square.py
@@ -1,7 +1,14 @@
 
+import numpy as np
+
+
 def square(n, fn = lambda x: x**2):
     """lambda function is power of 2"""
     return[fn(i) for i in range(n)]
 
-print(square(10))
 
+def sigmoid(x,a=1):
+    return 1/(1+np.exp(-a*x))
+
+print(square(10))
+print(sigmoid(0.0))
